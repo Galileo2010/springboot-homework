a02df07d05d7e38a33f4abc0c31eeb50d8c6b471
Galileo2010 <15549070600@163.com>
2019-04-01 00:16:33 +0800

    init commit

---
+++ src/main/java/com/homework/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.homework.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RequestMapping("/")
+	String index()
+	{
+		return "俊宝宝是最帅的！";
+	}
+}
